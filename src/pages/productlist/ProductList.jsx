7d360843dab0c8a2aa4e1574352038b6591969a6
alfygitau <alfygitau@gmail.com>
2022-03-04 11:13:24 +0300

    fixed pagination

--- src/pages/productlist/ProductList.jsx
+++ src/pages/productlist/ProductList.jsx
@@ -4,23 +4,37 @@ import Chips from "../../components/chips/chips";
 import Products from "../../components/productsCard/Products";
 import Search from "../../components/searchBar/SearchBar";
 import "./ProductList.css";
+import Pagination from "@mui/material/Pagination";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [show, setShow] = useState(true);
-  const [searchResults, setSearchResults] = useState([])
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(0);
+  const [searchResults, setSearchResults] = useState([]);
+
+  const url = `https://dummyjson.com/products?limit=${limit}&skip=0`;
 
   const fetchProducts = async () => {
-    await axios.get("https://dummyjson.com/products").then((response) => {
-      console.log(response.data.products);
+    await axios.get(url).then((response) => {
+      console.log("page", response.data.products);
       setProducts(response.data.products);
+      setCount(response.data.total);
     });
   };
 
+  const handleChange = async (event, value) => {
+    console.log("value", value);
+    setPage(value);
+    setLimit(value);
+  };
+  console.log("limit", limit);
+
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    fetchProducts(limit);
+  }, [limit]);
 
   const handleClick = async (category) => {
     console.info("You clicked the Chip", category);
@@ -39,17 +53,16 @@ const ProductList = () => {
       .then((response) => {
         console.log(response.data.products);
         setSearchResults(response.data.products);
-        setShow(false)
-        setFilteredProducts(response.data.products)
+        setShow(false);
+        setFilteredProducts(response.data.products);
       });
   };
-
   console.log("results", searchResults);
 
   return (
     <>
       <div className="category-chips">
-          <Search searchProducts={searchProducts}/>
+        <Search searchProducts={searchProducts} />
         <Chips handleClick={handleClick} />
       </div>
       <div className="productlist">
@@ -85,6 +98,18 @@ const ProductList = () => {
               id={product.id}
             />
           ))}
+        <div className="pagination">
+          <Pagination
+            className="my-3"
+            count={count}
+            page={page}
+            siblingCount={1}
+            boundaryCount={1}
+            variant="outlined"
+            shape="rounded"
+            onChange={handleChange}
+          />
+        </div>
       </div>
     </>
   );
