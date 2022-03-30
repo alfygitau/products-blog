2933c309adf9e4e9511116d230b25f8460c73fcb
alfygitau <alfygitau@gmail.com>
2022-03-30 23:41:46 +0300

    added a paypal payment method

--- src/other files/Paypal.jsx
+++ src/other files/Paypal.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Paypal = () => {
+  const total = useSelector((state) => state.cart.total);
+  const user = useSelector((state) => state.login.user.firstName);
+  const description = `${user}'s bag`;
+  console.log(description);
+  const [paidFor, setPaidFor] = useState(false)
+
+  const handleApprove = (orderId) => {
+    // send a request to the backend server
+    setPaidFor(true)
+  };
+
+  if(paidFor){
+      toast.success(`Payment by ${user} successful`)
+  }
+
   return (
     <PayPalScriptProvider
       options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
     >
-      <PayPalButtons />
+      <PayPalButtons
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: description,
+                amount: {
+                  value: total,
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={async (data, actions) => {
+          const order = await actions.order.capture();
+          console.log("order", order);
+          handleApprove(data.orderID);
+        }}
+      />
     </PayPalScriptProvider>
   );
 };
