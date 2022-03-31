5b342c39332febd60b0736e1b6cfee40e2cd873f
alfygitau <alfygitau@gmail.com>
2022-03-31 11:54:58 +0300

    added a related product section in the details page

--- src/components/productDetails/ProductDetails.jsx
+++ src/components/productDetails/ProductDetails.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { addToCart } from "../../redux/cartRedux";
 import "./ProductDetails.css";
+import Products from "../productsCard/Products";
 
 const ProductDetails = () => {
   const [product, setProduct] = useState({});
+  const [category, setCategory] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -26,40 +28,71 @@ const ProductDetails = () => {
     fetchProduct(id);
   }, [id]);
 
+  const fetchCategory = async (category) => {
+    category = product.category;
+    console.log(category);
+    const response = await axios.get(
+      `https://dummyjson.com/products/category/${category}`
+    );
+    setCategory(response.data.products);
+  };
+  useEffect(() => {
+    fetchCategory();
+  }, [product]);
+  console.log("category", category);
+
   const handleAddToCart = () => {
     dispatch(addToCart({ ...product }));
   };
 
   return (
-    <div className="product-details">
-      <div className="product-image">
-        <img src={product.thumbnail} alt="product" />
+    <>
+      <div className="product-details">
+        <div className="product-image">
+          <img src={product.thumbnail} alt="product" />
+        </div>
+        <div className="description">
+          <h4>Title: {product.title}</h4>
+          <h5>Brand: {product.brand}</h5>
+          <h5>Price: $ {product.price}</h5>
+          <h6>Category: {product.category}</h6>
+          <span>Available in Stock: {product.stock}</span>
+          <h6>Rating: {product.rating}</h6>
+          <p>Description: {product.description}</p>
+          <button
+            type="button"
+            onClick={handleAddToCart}
+            className="btn btn-primary"
+          >
+            ADD TO CART
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-dark"
+            onClick={() => navigate("/products")}
+            style={{ marginLeft: "10px" }}
+          >
+            Continue Shopping
+          </button>
+        </div>
       </div>
-      <div className="description">
-        <h4>Title: {product.title}</h4>
-        <h5>Brand: {product.brand}</h5>
-        <h5>Price: $ {product.price}</h5>
-        <h6>Category: {product.category}</h6>
-        <span>Available in Stock: {product.stock}</span>
-        <h6>Rating: {product.rating}</h6>
-        <p>Description: {product.description}</p>
-        <button
-          type="button"
-          onClick={handleAddToCart}
-          className="btn btn-primary"
-        >
-          ADD TO CART
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-dark"
-          onClick={() => navigate("/products")}
-          style={{marginLeft: "10px"}}
-        >
-          Continue Shopping
-        </button>
+      <h4 style={{ textAlign: "center", color: "rgb(204,46,67)" }}>
+        Related Products
+      </h4>
+      <div className="related">
+        {category?.map((item) => (
+          <Products
+            key={item.key}
+            title={item.title}
+            description={item.description}
+            brand={item.brand}
+            image={item.thumbnail}
+            category={item.category}
+            price={item.price}
+          />
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
