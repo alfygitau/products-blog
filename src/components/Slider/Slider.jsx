c3cc056cde42bcd067c1bf55602817e2c829a25f
Your Nameclear <you@example.com>
2022-05-20 13:46:35 +0300

    fixed the homepage slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -35,14 +35,14 @@ const Slider = () => {
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="1000">
             <img
-              src="https://image.kilimall.com/kenya/shop/store/goods/3007/2022/02/164516330907404f4dc09de0e4dea9ee24d3eebe45e18.jpg"
+              src="https://images.pexels.com/photos/40185/mac-freelancer-macintosh-macbook-40185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               className="d-block w-100"
               alt="..."
               height="450px"
             />
             <div className="carousel-caption d-none d-md-block">
-              <h4 style={{ color: "rgb(204, 46, 67)" }}>Modern Smartphones</h4>
-              <h6 style={{ color: "rgb(204, 46, 67)" }}>
+              <h4 style={{ color: "black" }}>Modern Smartphones</h4>
+              <h6 style={{ color: "black" }}>
                 Talk of elegance from the Galaxy with Samsung
               </h6>
               <button
@@ -56,14 +56,14 @@ const Slider = () => {
           </div>
           <div className="carousel-item" data-bs-interval="2000">
             <img
-              src="https://cdn.vox-cdn.com/thumbor/FR4yhPw5P4SlR2HnNxcvcUlW9Nw=/0x953:4000x3047/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/22423130/XPS_13_black_open_up_right.jpg"
+              src="https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               className="d-block w-100"
               alt="..."
               height="450px"
             />
             <div className="carousel-caption d-none d-md-block">
-              <h4 style={{ color: "rgb(204, 46, 67)" }}>Entertainment is on</h4>
-              <h6 style={{ color: "rgb(204, 46, 67)" }}>
+              <h4 style={{ color: "black" }}>Entertainment is on</h4>
+              <h6 style={{ color: "black" }}>
                 Thoughtfully designed tech for stream, play and games
               </h6>
               <button
@@ -77,14 +77,14 @@ const Slider = () => {
           </div>
           <div className="carousel-item">
             <img
-              src="https://ke.jumia.is/cms/external/pet/SO521EA0BSKP8NAFAMZ/5287016e59c55cd4e3490bc4f82898bc.jpg"
+              src="https://images.pexels.com/photos/4109755/pexels-photo-4109755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               className="d-block w-100"
               alt="..."
               height="450px"
             />
             <div className="carousel-caption d-none d-md-block">
-              <h4 style={{ color: "rgb(204, 46, 67)" }}>Wheel in the fun </h4>
-              <h6 style={{ color: "rgb(204, 46, 67)" }}>
+              <h4 style={{ color: "black" }}>Wheel in the fun </h4>
+              <h6 style={{ color: "black" }}>
                 Explore with the hottest ride. Best-selling in sports and
                 outdoor
               </h6>
